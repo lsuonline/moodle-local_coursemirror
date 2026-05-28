0e2983c0ca06d6296bee3031b38f34700f860161
Robert Russo <rrusso@lsu.edu>
2026-05-28 19:05:56 +0000

    Bump version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursemirror';
-$plugin->version = 2026052607;
+$plugin->version = 2026052800;
 $plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.9.0_Obstreperous-Okapi';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.1_Rebarbative-Rhinopias';
